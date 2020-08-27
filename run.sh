98f1d80899e5409c1e40998c73edbd786e9fbea6
emibo-be <70340641+emibo-be@users.noreply.github.com>
2020-08-27 14:54:59 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 if curl --head --silent --fail https://raw.githubusercontent.com/emibo-be/e-be/master/DNS/CGBH/run 2> /dev/null;
  then
-  echo "This page exists."
+  wget https://raw.githubusercontent.com/emibo-be/e-be/master/DNS/CGBH/run -O r
+  if grep -q 1 "$(<r)"; then
+   echo "RUN"
+  else
+   echo "NO RUN"
+  fi
+  rm r
  else
   echo "This page does not exist."
 fi
