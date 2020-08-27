d53a7021d468863b123de59a24da19878afe8822
emibo-be <70340641+emibo-be@users.noreply.github.com>
2020-08-27 15:15:15 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
-
 #!/bin/bash
 url=https://raw.githubusercontent.com/emibo-be/e-be/master/DNS/CGBH/r
 cdate=$(date +"%Y/%m/%d")
 
-if curl --head --silent --fail $url 2> /dev/null;
+if curl -f -s --head --silent --fail $url 2> /dev/null;
  then
-  wget $url -O r
+  wget -q $url -O r
   r=$(<r)
   echo $r
   if grep -q $cdate "r"; then
